9bc806aa98e00f4b6198caba65702f8dcbc7dad5
rhythamitaliya <ritaliya2003@gmail.com>
2024-05-09 11:17:56 +0530

    07-05-2024

--- src/common/Loader/index.tsx
+++ src/common/Loader/index.tsx
@@ -22,5 +22,4 @@ export const LittlelLoader = () => {
       <div className="h-4 w-4 animate-spin rounded-full border-2 border-solid border-primary border-t-transparent"></div>
     </div>
   );
-
 };
